740ff484f79c4a3462c161fe464d1c799c744062
JOTTV <99305833+JOTTV@users.noreply.github.com>
2022-02-23 10:44:44 -0500

    Add files via upload

---
+++ Ejercicio N°2/punto 2.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Boolean A = true, B = true, C = true, D = false, E = false;
+            if (A)
+            {
+                if (B)
+                {
+                    Console.WriteLine("Secuanecia 1");
+                }
+                else
+                {
+                    if (C)
+                    {
+                        Console.WriteLine("Secuanecia 2");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Secuanecia 3");
+                    }
+                }
+            }
+            else
+            {
+                Console.WriteLine("Secuanecia 4");
+            }
+            if (D)
+            {
+                if (E)
+                {
+                    Console.WriteLine("Secuanecia 5");
+                }
+                else
+                {
+                    Console.WriteLine("Secuanecia 6");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Secuanecia 7");
+            }
+        }
+    }
+}
